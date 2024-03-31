ecf7806905a77c1bcc30d5b028d0d4f4058a03da
zakaria el hajjioui <zakaria.elhajjioui@student.rocva.nl>
2024-03-31 16:13:22 +0200

    database tabel aanmaken en dummy content er in zetten

---
+++ database/migrations/2024_03_29_094552_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('category_id');
+            $table->string('title');
+            $table->string('slug')->nullable();
+            $table->longText('descriptions')->nullable();
+            $table->decimal('price', 8,2);
+            $table->string('file_name')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,61 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Category;
+use App\Models\Product;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
+        $category = new Category;
+        $category->title = 'Heren';
+        $category->slug = 'heren';
+        $category->save();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $category = new Category;
+        $category->title = 'Dames';
+        $category->slug = 'dames';
+        $category->save();
+
+        $category = new Category;
+        $category->title = 'Kinderen';
+        $category->slug = 'kinderen';
+        $category->save();
+
+        $product = new Product;
+        $product->category_id = 1;
+        $product->title = 'Test Product 1';
+        $product->slug = 'test_product_1';
+        $product->price = 25;
+        $product->save();
+
+        $product = new Product;
+        $product->category_id = 1;
+        $product->title = 'Test Product 2';
+        $product->slug = 'test_product_2';
+        $product->price = 25;
+        $product->save();
+
+        $product = new Product;
+        $product->category_id = 1;
+        $product->title = 'Test Product 3';
+        $product->slug = 'test_product_3';
+        $product->price = 25;
+        $product->save();
+
+        $product = new Product;
+        $product->category_id = 2;
+        $product->title = 'Test Product 4';
+        $product->slug = 'test_product_4';
+        $product->price = 25;
+        $product->save();
     }
 }
